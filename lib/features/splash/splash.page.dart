0e16b5d30fb8a85fff17a44ee03a3449bb084f6a
Rafael Jordao <rafaeljordao.b@gmail.com>
2024-07-11 20:59:58 -0300

    update: adiciona comportament de nevagação para a tela onboarding

--- lib/features/splash/splash.page.dart
+++ lib/features/splash/splash.page.dart
@@ -1,9 +1,40 @@
+import 'dart:async';
+
 import 'package:financy_app/common/constants/app_colors.dart';
 import 'package:financy_app/common/constants/app_text_styles.dart';
+import 'package:financy_app/common/constants/routes.dart';
+import 'package:financy_app/common/widgets/custom_circular_progrees_indicator.dart';
+import 'package:financy_app/features/onboarding/onboarding_page.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    init();
+  }
+
+  Timer init() {
+    return Timer(
+      const Duration(seconds: 2),
+      navigateToOnboarding,
+    );
+  }
+
+  void navigateToOnboarding() {
+    Navigator.pushReplacementNamed(
+      context,
+      NamedRoutes.initial,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +46,15 @@ class SplashPage extends StatelessWidget {
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
                 colors: AppColors.greenGradient)),
-        child: Text(
-          'financy',
-          style: AppTextStyles.bigText.copyWith(color: AppColors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'financy',
+              style: AppTextStyles.bigText.copyWith(color: AppColors.white),
+            ),
+            const CustomCircularProgressIndicator(),
+          ],
         ),
       ),
     );
