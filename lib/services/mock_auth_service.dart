02e97eda22ebb28bcbb0b5bcda1245714396dc91
Rafael Jordao <rafaeljordao.b@gmail.com>
2024-07-10 21:09:01 -0300

    mock: implementa mock auth service

---
+++ lib/services/mock_auth_service.dart
@@ -0,0 +1,31 @@
+import 'package:financy_app/common/constants/models/user_model.dart';
+import 'package:financy_app/services/auth_service.dart';
+
+class MockAuthService implements AuthService {
+  @override
+  Future signIn() => throw UnimplementedError();
+
+  @override
+  Future<UserModel> signUp({
+    String? name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await Future.delayed(const Duration(seconds: 2));
+      if (password.startsWith('123')) {
+        throw Exception();
+      }
+      return UserModel(
+        id: email.hashCode,
+        name: name,
+        email: email,
+      );
+    } catch (e) {
+      if (password.startsWith('123')) {
+        throw 'Senha insegura. Digite uma senha forte.';
+      }
+      throw 'Não foi possível criar sua conta nessse momento. Tente mais tarde.';
+    }
+  }
+}
