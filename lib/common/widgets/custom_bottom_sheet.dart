2326aa1b6b10fe74b39c97d0ab3d0b146f6f6ecb
Rafael Jordao <rafaeljordao.b@gmail.com>
2024-07-10 21:07:04 -0300

    refactor: ajusta customBottomSheet

--- lib/common/widgets/custom_bottom_sheet.dart
+++ lib/common/widgets/custom_bottom_sheet.dart
@@ -3,7 +3,12 @@ import 'package:financy_app/common/constants/app_text_styles.dart';
 import 'package:financy_app/common/widgets/primary_button.dart';
 import 'package:flutter/material.dart';
 
-Future<dynamic> customModalBottomSheet(BuildContext context) {
+Future<void> customModalBottomSheet(
+  BuildContext context, {
+  required String content,
+  required String buttonText,
+  VoidCallback? onPressed,
+}) {
   return showModalBottomSheet(
     context: context,
     shape: const RoundedRectangleBorder(
@@ -17,26 +22,31 @@ Future<dynamic> customModalBottomSheet(BuildContext context) {
                 topLeft: Radius.circular(38.0),
                 topRight: Radius.circular(38.0))),
         height: 200,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                'Ops. Algo deu errado...',
-                style: AppTextStyles.mediumText20
-                    .copyWith(color: AppColors.greenOne),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 16.0, horizontal: 32.0),
-                child: PrimaryButtom(
-                  text: "Tentar Novamente",
-                  onPressed: () => Navigator.pop(context),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Text(
+                content,
+                style: AppTextStyles.mediumText20.copyWith(
+                  color: AppColors.greenOne,
                 ),
               ),
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 8.0,
+                horizontal: 32.0,
+              ),
+              child: PrimaryButtom(
+                text: buttonText,
+                onPressed: onPressed ?? () => Navigator.pop(context),
+              ),
+            ),
+          ],
         ),
       );
     },
