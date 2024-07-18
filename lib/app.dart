bcd5a75bc5ddb1b05bfb2abae0469c9017d6d2fc
Rafael Jordao <rafaeljordao.b@gmail.com>
2024-07-18 20:47:26 -0300

    update: adiciona rota de sign no app

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:financy_app/common/constants/routes.dart';
 import 'package:financy_app/features/onboarding/onboarding_page.dart';
 import 'package:financy_app/common/constants/themes/default_theme.dart';
 import 'package:financy_app/features/sign_up/sign_up_page.dart';
+import 'package:financy_app/features/sing_in/sing_in_page.dart';
 import 'package:financy_app/features/splash/splash.page.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +18,7 @@ class App extends StatelessWidget {
         NamedRoutes.initial: (context) => const OnboardingPage(),
         NamedRoutes.splash: (context) => const SplashPage(),
         NamedRoutes.signUp: (context) => const SignUpPage(),
+        NamedRoutes.signIn: (context) => const SignInPage(),
       },
     );
   }
