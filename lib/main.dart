fad8fbb7724418b93d43dab4601d852738a95b0f
Rafael Jordao <rafaeljordao.b@gmail.com>
2024-07-27 11:41:20 -0300

    feat: firebase configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:financy_app/features/locator.dart';
 
 import 'app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   setupDependencies();
   runApp(const App());
 }
